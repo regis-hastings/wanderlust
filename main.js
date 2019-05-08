3d444e340b061f3dae9f1ce434e4c217225c72ac
Regis Hastings <regis@Regiss-iMac.local>
2019-05-08 13:01:01 -0700

    finished getVenues function

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 // Foursquare API Info
 const clientId = '';
 const clientSecret = '';
-const url = '';
+const url = 'https://api.foursquare.com/v2/venues/explore?near=';
 
 // APIXU Info
 const apiKey = '';
-const forecastUrl = '';
+const forecastUrl = 'https://api.apixu.com/v1/forecast.json?key=&q=';
 
 // Page Elements
 const $input = $('#city');
@@ -17,12 +17,38 @@ const $weatherDivs = [$("#weather1"), $("#weather2"), $("#weather3"), $("#weathe
 const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 // Add AJAX functions here:
-const getVenues = () => {
-
+const getVenues = async () => {
+  
+  const city = $input.val();
+  const urlToFetch = `${url}${city}&limit=10&client_id=${clientId}&client_secret=${clientSecret}&v=20190508`;
+  
+  try {
+    const response = await fetch(urlToFetch);
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      const venues = jsonResponse.response.groups[0].items.map(item => item.venue);
+      return venues;
+    }
+  } catch(error) {
+    console.log(error);
+  }
+  
 }
 
-const getForecast = () => {
-
+const getForecast = async () => {
+  
+  const city = $input.val();
+  const urlToFetch = `${forecastUrl}${city}&days=4&hour=11`;
+  
+  try {
+    const response = await fetch(urlToFetch);
+    if (response.ok) {
+      
+    }
+  } catch(error) {
+    console.log(error)
+  }
+  
 }
 
 
