9c2666eb969285af65d9d022ddbb9cf7bc5b000b
Regis <25518602+regis-hastings@users.noreply.github.com>
2019-05-09 11:12:08 -0700

    finished extra credit challenge 2: add additional info about weather (added humidity)

--- helpers.js
+++ helpers.js
@@ -10,6 +10,7 @@ const createVenueHTML = (name, location, iconSource) => {
 const createWeatherHTML = (currentDay) => {
   return `<h2> High: ${currentDay.day.maxtemp_f}</h2>
     <h2> Low: ${currentDay.day.mintemp_f}</h2>
+    <h2> Average Humidity: ${currentDay.day.avghumidity}</h2>
     <img src="https://${currentDay.day.condition.icon}" class="weathericon" />
     <h2>${weekDays[(new Date(currentDay.date)).getDay()]}</h2>`;
 }
